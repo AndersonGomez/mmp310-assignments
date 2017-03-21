c066387d6b2cdc762448351c97c6deec0a6219c0
Anderson Gomez <Gomez.anderson16@gmail.com>
2017-03-21 19:52:39 -0400

    variable div

--- assignment-03/js/app.js
+++ assignment-03/js/app.js
@@ -1,2 +1,7 @@
+var div = {
+ +    element: document.getElementsByTagName('div')[0],
+ +    height: 300,
+ +    width: 300
+ +}
 /*jslint browser, es6, single, for, devel, this */
 /*global window */
